f19f47583e2915ffea38f4b3832b78a576e2eb38
Michelle Steigerwalt <msteigerwalt@gmail.com>
2016-10-26 19:22:30 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+config = {
+    'description': "🐻 Terminal library on top of bearlibterminal.",
+    'author': "Michelle Steigerwalt",
+    'author_email': "msteigerwalt@gmail.com",
+    'url': 'http://github.com/Yuffster/beary',
+    'version': VERSION,
+    'packages': find_packages(exclude=["tests"]),
+    'name': 'beary',
+    'test_suite': 'nose.collector',
+    'zip_safe': True,
+    'install_requires':[
+        'bearlibterminal',
+    ]
+}
+
+setup(**config)
